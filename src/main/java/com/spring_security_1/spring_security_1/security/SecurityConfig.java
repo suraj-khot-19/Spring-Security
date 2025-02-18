9a6b269a9664f3ddd6120cbc853d1063c878bbb8
suraj-khot-19 <surajkhot19082001@gmail.com>
2025-02-18 18:01:20 +0530

    config security customizations

---
+++ src/main/java/com/spring_security_1/spring_security_1/security/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.spring_security_1.spring_security_1.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> ((AuthorizeHttpRequestsConfigurer.AuthorizedUrl)requests.anyRequest()).authenticated());
+
+        /// form based login
+        //http.formLogin(Customizer.withDefaults());
+
+        /// basic alert based login
+        http.httpBasic(Customizer.withDefaults());
+        return (SecurityFilterChain)http.build();
+    }
+}
