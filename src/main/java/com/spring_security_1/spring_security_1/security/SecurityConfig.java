57cb6374b90edcbe3f3235791fc7c8d9196ddf8b
suraj-khot-19 <surajkhot19082001@gmail.com>
2025-02-18 18:08:38 +0530

    setting application as a stateless with help of sessionCreationPolicy

--- src/main/java/com/spring_security_1/spring_security_1/security/SecurityConfig.java
+++ src/main/java/com/spring_security_1/spring_security_1/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,6 +13,11 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((requests) -> ((AuthorizeHttpRequestsConfigurer.AuthorizedUrl)requests.anyRequest()).authenticated());
+
+        /// this will make application which won't store cookies(STATELESS)
+        /// otherwise it always store "JSESSIONID" in browser storage
+        http.sessionManagement(session
+                ->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         /// form based login
         //http.formLogin(Customizer.withDefaults());
